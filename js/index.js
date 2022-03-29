337623702d11563c0ecbd46c91f6756c8c67cd1a
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-03-29 21:33:27 +0100

    edited facebook demo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 let profile=document.getElementsByClassName("profile_picture")[0];
-let name = document.getElementById("name");
+let profile2=document.getElementsByClassName("profile_picture")[1];
+let profile3=document.getElementsByClassName("profile_picture")[2];
+let name = document.getElementsByClassName("name")[0];
+let name2 = document.getElementsByClassName("name")[1];
+let name3 = document.getElementsByClassName("name")[2];
+let postPicture=document.getElementsByClassName("postPicture")[0];
+let postPicture2=document.getElementsByClassName("postPicture")[1];
+let postPicture3=document.getElementsByClassName("postPicture")[2];
 const getUsers = async() =>{
     let res = await axios.get('https://randomuser.me/api/')
     const array= res.data.results[0];
@@ -8,7 +15,34 @@ const getUsers = async() =>{
     gottenArray=JSON.parse(localStorage.getItem('nam'))
     console.log(gottenArray)
     profile.setAttribute("src", `${gottenArray.picture.medium}`);  
+    postPicture.setAttribute("src", `${gottenArray.picture.large}`);  
     name.innerHTML=`${gottenArray.name.first} ${gottenArray.name.last}` 
 }
 getUsers();
 
+const getUsers2 = async() =>{
+    let res = await axios.get('https://randomuser.me/api/')
+    const array= res.data.results[0];
+    console.log(array)
+    localStorage.setItem('nam', JSON.stringify(array))
+    gottenArray=JSON.parse(localStorage.getItem('nam'))
+    console.log(gottenArray)    
+    profile2.setAttribute("src", `${gottenArray.picture.medium}`);  
+    postPicture2.setAttribute("src", `${gottenArray.picture.large}`);     
+    name2.innerHTML=`${gottenArray.name.first} ${gottenArray.name.last}` 
+}
+getUsers2();
+
+let numbersOfThumbsup = document.getElementById("numbersOfThumbsup")
+console.log(numbersOfThumbsup);
+numbersOfThumbsup.textContent=0;
+
+
+
+document.querySelector("#thumbsUp1").addEventListener("click", function(e){
+    if (numbersOfThumbsup.textContent == '0') {
+        numbersOfThumbsup.textContent++   
+    } else {
+        numbersOfThumbsup.textContent = 0
+    }
+})
