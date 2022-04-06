a9c09c8cd5d4d4998b65df997d21b2ad52b5ec2f
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-04-06 15:28:21 +0100

    added event listeners to thumbsUp and comments

--- js/index.js
+++ js/index.js
@@ -38,89 +38,69 @@ const fetchUser = async () => {
      <div>
        <i class="fa-solid fa-thumbs-up text-white bg-primary rounded-circle p-2"></i>
        <i class="fa-solid fa-heart text-white bg-danger rounded-circle p-2 marginForThumbsup border border-4 border-light"></i>
-       <span class="numbersOfThumbsup">105</span>
+       <span class="numbersOfThumbsup">0</span>
      </div>
      <div class="align-self-center"><span>85</span><span> comments</span></div>
    </div>
    <hr>
 
    <div class="d-flex container justify-content-between">
-     <div><i class="fa-regular fa-thumbs-up thumbsUp1"></i><span class="numbersOfThumbsup">105</span></div>
-     <div><i class="fa-regular fa-message"></i><span> Comments</span></div>
+     <div><i class="fa-regular fa-thumbs-up thumbsUp1"></i><span class="numbersOfThumbsup">0</span></div>
+     <div><i class="fa-regular fa-message"></i><span class="comments"> Comments</span></div>
      <div><i class="fa-solid fa-share"></i><span> share</span></div>
    </div>
+    <div class="d-none commentSection">
+    <div class="postedComments ms-2 my-1"></div>
+    <div class="input-group my-2">      
+       <input type="text" class="form-control border-0 bg-light inputComment" placeholder="write comment here">
+       <button class="btn btn-light commentBtn" type="button" id="button-addon2"><i class="fa-solid fa-paper-plane text-secondary"></i></button>
+     </div>
+    </div>
+   
    <hr>
    
       </div>
         `;
        
-        randDiv.innerHTML = template;
-        peopleElem.appendChild(randDiv);  
-        let numbersOfThumbsup = document.querySelectorAll(".numbersOfThumbsup")
-        let thumbsUp1 = document.querySelector(".thumbsUp1") 
-        console.log(thumbsUp1)
-        thumbsUp1.addEventListener('click', function(e){
-            console.log(99);
-            // if (numbersOfThumbsup.classList.contains('liked')) {
-            //                     let count = Number(element.target.innerText)
-            //                     count--;
-            //                     element.target.innerText = count.toString();
-            //                     element.target.classList.remove('liked')
-            //                 } else {
-            //                     let count = Number(element.target.innerText)
-            //                     count++;
-            //                     element.target.innerText = count.toString();
-            //                     element.target.classList.add('liked')
-                                
-            //                 }
+       randDiv.innerHTML = template;
+        peopleElem.appendChild(randDiv); 
+      
+        // adding event listener to thumbsUp and comments
+        randDiv.addEventListener('click', function(e){
+          let numbersOfThumbsup = randDiv.getElementsByClassName('numbersOfThumbsup')[0]
+          let numbersOfThumbsup2 = randDiv.getElementsByClassName('numbersOfThumbsup')[1]
+         if(e.target.classList.contains('thumbsUp1') && numbersOfThumbsup.textContent == '0'){
+                    numbersOfThumbsup.textContent++;
+                    numbersOfThumbsup2.textContent=numbersOfThumbsup.textContent;
+                    e.target.classList.add('text-danger')
+                    
+                } else {
+                    numbersOfThumbsup.textContent = 0;
+                    numbersOfThumbsup2.textContent=numbersOfThumbsup.textContent; 
+                    e.target.classList.remove('text-danger')
+                }
+          if(e.target.classList.contains('commentBtn') || e.target.classList.contains('fa-paper-plane') ){
+            let postedComments=randDiv.getElementsByClassName('postedComments')[0];
+            let inputComment=randDiv.getElementsByClassName('inputComment')[0];
+            let commentText = document.createElement('p');
+            commentText.innerHTML=inputComment.value
+            inputComment.value=''
+            postedComments.appendChild(commentText)
+          }
+          if(e.target.classList.contains('fa-message') || e.target.classList.contains('comments')){
+          let commentSection=randDiv.getElementsByClassName('commentSection')[0];
+          let message=randDiv.getElementsByClassName('fa-message')[0];
+          commentSection.classList.toggle('d-none');
+          message.classList.toggle('text-primary')
+        }
+         
         })
 
-    //     let numbersOfThumbsup = document.querySelectorAll(".numbersOfThumbsup")
-    //     console.log(numbersOfThumbsup);
-    //     numbersOfThumbsup.forEach(element => {
-    //         element.onclick = function (element) {
-    //             if (element.target.classList.contains('liked')) {
-    //                 let count = Number(element.target.innerText)
-    //                 count--;
-    //                 element.target.innerText = count.toString();
-    //                 element.target.classList.remove('liked')
-    //             } else {
-    //                 let count = Number(element.target.innerText)
-    //                 count++;
-    //                 element.target.innerText = count.toString();
-    //                 element.target.classList.add('liked')
-                    
-    //             }
-               
-                
-    //         }
+      
+     
+     
+   }}
         
-    // });
-    }
-}
-
-
-fetchUser();
-
-
-
-
-
-
-// for (let index = 0; index < numbersOfThumbsup.length; index++) {
-//     console.log(numbersOfThumbsup[index]);   
-// }
-
-
-
-
-
-
-
-
-
-// let numbersOfThumbsup2 = document.getElementsByClassName("numbersOfThumbsup")[1]
-
-// numbersOfThumbsup.textContent=0;
-// numbersOfThumbsup2.textContent=0;
+        
+       fetchUser();
 
